a755d6bc3f58aaf6ded399752298f6d0b9e19aae
lolicer <13549177+lolicer@user.noreply.gitee.com>
2025-07-07 17:41:59 +0800

    修改了媒体控制逻辑

--- composeApp/src/desktopMain/kotlin/pers/lolicer/wotascope/components/bottomController/MediaState.kt
+++ composeApp/src/desktopMain/kotlin/pers/lolicer/wotascope/components/bottomController/MediaState.kt
@@ -5,5 +5,6 @@ import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer
 data class MediaState(
     val mediaPlayer: EmbeddedMediaPlayer,
     var isPlaying: Boolean,
-    var isFinished: Boolean
+    var isFinished: Boolean,
+    var isSelected: Boolean
 )
