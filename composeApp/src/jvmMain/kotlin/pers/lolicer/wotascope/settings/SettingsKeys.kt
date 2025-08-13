ecc2304ff33b3660b18a1deb7bb28f694f788fc8
lolicer <13549177+lolicer@user.noreply.gitee.com>
2025-08-13 23:24:01 +0800

    修复 当用户再次安装软件到其他位置时，默认路径不会改变的BUG

--- composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/settings/SettingsKeys.kt
+++ composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/settings/SettingsKeys.kt
@@ -1,6 +1,7 @@
 package pers.lolicer.wotascope.settings
 
 object SettingsKeys {
+    const val IS_FIRST_LAUNCH = "is-first-launch"
     const val PRE_ENCODING = "pre-encoding"
     const val ENCODED_VIDEO_DIR = "encoded-video-dir"
     const val MAIN_WINDOW_WIDTH = "main-window-width"
--- composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/settings/SettingsManager.kt
+++ composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/settings/SettingsManager.kt
@@ -19,11 +19,23 @@ object SettingsManager {
     }
 
     private fun Settings.initDefaultValues(){
+        // 关于：IS_FIRST_LAUNCH
+        // 若无此项设置，只能保证初次安装本应用的机器，默认目录会被定位到正确位置
+        // 添加此项设置，会通过安装目录下有没有temp_videos文件夹来判断此次启动是不是当前版本的初次启动，保证了用户安装新版本且安装到了其他位置时的默认路径的正确性
+
+        if(!contains(SettingsKeys.IS_FIRST_LAUNCH)) {
+            this[SettingsKeys.IS_FIRST_LAUNCH] = true
+        }
+        if(!File(System.getProperty("user.dir"), "temp_videos").exists()){
+            this[SettingsKeys.IS_FIRST_LAUNCH] = true
+        }
+
         if(!contains(SettingsKeys.PRE_ENCODING)) {
             this[SettingsKeys.PRE_ENCODING] = true
         }
-        if (!contains(SettingsKeys.ENCODED_VIDEO_DIR)) {
+        if (!contains(SettingsKeys.ENCODED_VIDEO_DIR) || this.getBooleanOrNull(SettingsKeys.IS_FIRST_LAUNCH)!!) {
             this[SettingsKeys.ENCODED_VIDEO_DIR] = File(System.getProperty("user.dir"), "temp_videos").path
+            this[SettingsKeys.IS_FIRST_LAUNCH] = false
         }
         if(!contains(SettingsKeys.MAIN_WINDOW_WIDTH)) {
             this[SettingsKeys.MAIN_WINDOW_WIDTH] = 800
