5753465458e74f5adc938f06833226737fb95565
lolicer <13549177+lolicer@user.noreply.gitee.com>
2025-08-18 17:08:01 +0800

    修复 isParagraph == true时描述文字无法显示的BUG

--- composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/components/DescriptionText.kt
+++ composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/components/DescriptionText.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -28,7 +27,7 @@ fun DescriptionText(
     ) {
         Text(
             modifier = modifier,
-            text = if(isParagraph)"    " else "" + text,
+            text = (if(isParagraph)"    " else "") + text,
             color = Color.Gray,
             fontSize = 13.sp
         )
