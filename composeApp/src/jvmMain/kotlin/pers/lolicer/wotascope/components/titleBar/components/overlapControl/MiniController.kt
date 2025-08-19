b9480bf582fd14260184295d72ee106753d56a52
lolicer <13549177+lolicer@user.noreply.gitee.com>
2025-08-19 12:05:08 +0800

    添加 使用模拟摇杆拖动、缩放和复位视频

--- composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/components/titleBar/components/overlapControl/MiniController.kt
+++ composeApp/src/jvmMain/kotlin/pers/lolicer/wotascope/components/titleBar/components/overlapControl/MiniController.kt
@@ -1,11 +1,17 @@
 package pers.lolicer.wotascope.components.titleBar.components.overlapControl
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -13,28 +19,38 @@ import androidx.compose.foundation.onClick
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import pers.lolicer.wotascope.components.BiliSlider
 import pers.lolicer.wotascope.components.DescriptionText
 import pers.lolicer.wotascope.status.MediaPlayerListStatus
 import pers.lolicer.wotascope.status.alpha
+import pers.lolicer.wotascope.status.offset
+import pers.lolicer.wotascope.status.scale
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer
 import wotascope.composeapp.generated.resources.Res
 import wotascope.composeapp.generated.resources.menu_circle
-import wotascope.composeapp.generated.resources.menu_circle_outline
+import wotascope.composeapp.generated.resources.menu_square
 import wotascope.composeapp.generated.resources.settings_notice
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -72,7 +88,7 @@ fun MiniController(
 
         DescriptionText(
             showDescription = showDescription,
-            text = "拖动或滚动左侧圆形区域以移动或缩放视频；\n点击或拖动右侧滑块以调整视频透明度。",
+            text = "拖动或滚动左侧圆形区域以移动或缩放视频；\n双击左侧圆形区域以重置视频位置；\n点击或拖动右侧滑块以调整视频透明度。",
             modifier = Modifier.padding(start = 4.dp, end = 0.dp, bottom = 12.dp),
             isParagraph = false
         )
@@ -83,12 +99,20 @@ fun MiniController(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun MiniControllerItem(
     mediaPlayer: EmbeddedMediaPlayer,
     height: Dp
 ){
-    var alpha by remember { mutableStateOf(mediaPlayer.alpha) }
+    val scope = rememberCoroutineScope()
+
+    var alpha by remember { mutableStateOf(mediaPlayer.alpha) } // 视频透明度
+
+    var joystickOffset by remember { mutableStateOf(Animatable(Offset(0f, 0f), Offset.VectorConverter)) }
+    val radius = height.value * 5/8 // 摇杆可移动的半径
+
+    var isDragging by remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier.height(height).fillMaxWidth(),
@@ -99,13 +123,48 @@ fun MiniControllerItem(
             contentAlignment = Alignment.Center
         ){
             Icon(
-                modifier = Modifier.size(height * 1/2).zIndex(0f),
-                painter = painterResource(Res.drawable.menu_circle_outline),
+                modifier = Modifier.size(height * 5/8).zIndex(0f),
+                painter = painterResource(Res.drawable.menu_square),
                 contentDescription = "",
                 tint = Color.LightGray
             )
             Icon(
-                modifier = Modifier.size(height * 3/8).zIndex(1f),
+                modifier = Modifier
+                    .size(height * 3/8)
+                    .zIndex(1f)
+                    .pointerHoverIcon(PointerIcon.Hand)
+                    .offset(
+                        joystickOffset.value.x.coerceIn(-radius, radius).dp,
+                        joystickOffset.value.y.coerceIn(-radius, radius).dp
+                    )
+                    .onDrag(
+                        onDragStart = {
+                            isDragging = true
+                        },
+                        onDrag = {
+                            joystickOffset = Animatable(joystickOffset.value + it, Offset.VectorConverter)
+                            // mediaPlayer.offset += it
+                        },
+                        onDragEnd = {
+                            isDragging = false
+                            scope.launch {
+                                joystickOffset.animateTo(
+                                    targetValue = Offset(0f, 0f),
+                                    animationSpec = tween(durationMillis = 250) // 300ms 动画
+                                )
+                            }
+                        }
+                    )
+                    .combinedClickable(
+                        onClick = {},
+                        onDoubleClick = {
+                            mediaPlayer.offset = Offset(0f, 0f)
+                        }
+                    )
+                    .onPointerEvent(PointerEventType.Scroll){
+                        val zoomDelta = it.changes.first().scrollDelta.y
+                        mediaPlayer.scale = (mediaPlayer.scale * (1f - zoomDelta * 0.1f))
+                    },
                 painter = painterResource(Res.drawable.menu_circle),
                 contentDescription = "",
                 tint = Color.LightGray
@@ -120,5 +179,17 @@ fun MiniControllerItem(
             },
             onValueChangeFinished = {}
         )
+
+        LaunchedEffect(isDragging){
+            if(isDragging){
+                while(true){
+                    withFrameNanos{
+                        val x = joystickOffset.value.x / 10
+                        val y = joystickOffset.value.y / 10
+                        mediaPlayer.offset += Offset(x, y)
+                    }
+                }
+            }
+        }
     }
 }
